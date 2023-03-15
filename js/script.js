3d8d634a4ac0fca06ef3db6426fd2fffaccfa66f
Marcus Vinicius <marcusv892@gmail.com>
2023-03-14 22:28:02 -0300

    Insert img icons reference weather city, and estructure logic return api

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const umidityElement = document.querySelector('#humidity span')
 const rainElement = document.querySelector('#rain span')
 const nameCityElement = document.querySelector('#name-city')
 const nameStateElement = document.querySelector('#country-city')
-
+const imgWeather = document.getElementById('#image-weather')
 
 const apikey = '07187a0404001db9ea0153b19efb1d22'
 
@@ -30,15 +30,15 @@ function validInput(city){
     getDataGeolocationApi(city)
 }
 
-async function getDataGeolocationApi(city) {
+ async function getDataGeolocationApi(city) {
     const apiGeolocationURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apikey}`
 
-    const dataApi = await fetch(apiGeolocationURL)
+    const dataApi =  await fetch(apiGeolocationURL)
     .then(data => data.json())
-    .catch(e => alert('Perca de conexão com servidor', e))
+    .catch(e => alert('Connection server error', e))
 
     if(dataApi == ''){
-        alert('Cidade não encontrada')
+        alert('City not found')
         return
     }
 
@@ -49,25 +49,57 @@ async function getDataGeolocationApi(city) {
 }
 
 
-const getDataWeather = async(dataCity) => {
+ async function getDataWeather (dataCity){
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${dataCity.lat}&lon=${dataCity.lon}&units=metric&appid=${apikey}&lang=pt_br`
 
-    const dataApi = await fetch(apiWeatherURL)
-    const dataWeatherCity = await dataApi.json()
+    const dataCityWeather = await fetch(apiWeatherURL).then(data => data.json()).catch(e => alert('Connection server error', e))
 
-   showWatherDataApi(dataWeatherCity, dataCity)
+   showWatherDataApi(dataCityWeather, dataCity)
 }
 
-const showWatherDataApi = async(dataWeatherCity, dataCity) => {
-    console.log(dataWeatherCity, dataCity)
+async function showWatherDataApi(dataCityWeather, dataCity){
+    console.log(dataCityWeather)
 
-    nameCityElement.innerText = dataCity.name
-    nameStateElement.innerText = dataCity.country
-    tempElement.innerText = parseInt(dataWeatherCity.main.temp)
-    tempMaxElement.innerText = parseInt(dataWeatherCity.main.temp_max)
-    tempMinElement.innerText = parseInt(dataWeatherCity.main.temp_min)
-    windSpeedElement.innerText = dataWeatherCity.wind.speed
-    umidityElement.innerText = dataWeatherCity.main.humidity
+    const { name, country } = dataCity
+    const { main: { temp, temp_max, temp_min, humidity }, wind: { speed, deg }, weather: [{ main }] } = dataCityWeather
+
+    switch(main){
+        case 'Clouds':
+            imgWeather.src = 'assets/cloud.svg'
+            break
+
+        case ('Clear'): 
+            imgWeather.src = 'assets/sun.svg'
+            break
+
+        case ('Snow'):
+            imgWeather.src = 'assets/snowflake.svg'
+            break
+
+        case('Rain'):
+            imgWeather.src = 'assets/rainimg.svg'
+            break
+
+        case('Drizzle'):
+            imgWeather.src = 'assets/drizzle.svg'
+            break
+            
+        case('Thunderstorm'):
+            imgWeather.src = 'assets/thunderstorm.svg'
+            break
+            
+        default:
+            imgWeather.src = ''
+    }
+
+    nameCityElement.innerText = name
+    nameStateElement.innerText = country
+    tempElement.innerText = parseInt(temp)
+    tempMaxElement.innerText = parseInt(temp_max)
+    tempMinElement.innerText = parseInt(temp_min)
+    windSpeedElement.innerText = speed
+    umidityElement.innerText = humidity
+    rainElement.innerText = deg
 }
 
    
